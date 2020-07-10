f282b2e9822eb748d1fecf07effbbe3bd8acaae8
ptyin <peteryin1604@gmail.com>
2020-07-10 20:21:09 +0800

    change train epoch
    dataset			epoch
    ---------------------------------------
    automotive		500
    cellphones		50
    clothing		50
    digital music	50
    electronics		50
    toys			500

---
+++ evaluation.sh
@@ -0,0 +1,7 @@
+
+python main.py --setting_file "config/automotive/lse.yaml"
+python main.py --setting_file "config/cellphones/lse.yaml"
+python main.py --setting_file "config/clothing/lse.yaml"
+python main.py --setting_file "config/digital_music/lse.yaml"
+python main.py --setting_file "config/electronics/lse.yaml"
+python main.py --setting_file "config/toys/lse.yaml"
