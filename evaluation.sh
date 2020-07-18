08e41c7a5e3f198d8c3d75083dc1cf5799d9a7ee
ptyin <peteryin1604@gmail.com>
2020-07-18 19:55:07 +0800

    update evaluation.sh

--- evaluation.sh
+++ evaluation.sh
@@ -4,11 +4,11 @@ models=("lse" "hem" "aem" "zam")
 embedding_size="embed+0"
 
 echo "model,mrr,ndcg20" > "${embedding_size}.csv"
-for (( i = 0; i < 1; i++ )); do
+for (( i = 0; i < 6; i++ )); do
   for (( j = 0; j < 4; j++ )); do
-    echo "${models[j]}," | tr '\n' '' >> "${embedding_size}.csv"
-    ./galago-3.16-bin/bin/galago eval --judgments= "/home/share/yinxiangkun/transformed/${dataset[i]}/seq_min_count5/seq_query_split/test.qrels" --runs+ "/home/share/yinxiangkun/saved/${embedding_size}/${dataset[i]}/${models[j]}.ranklist"  --metrics+recip_rank --metrics+ndcg20 | awk 'NR==2{print $2}' | tr '\n' '' >> "${embedding_size}.csv"
+    echo "${models[j]}," | tr -d '\n'>> "${embedding_size}.csv"
+    ./galago-3.16-bin/bin/galago eval --judgments= "/home/share/yinxiangkun/transformed/${dataset[i]}/seq_min_count5/seq_query_split/test.qrels" --runs+ "/home/share/yinxiangkun/saved/${embedding_size}/${dataset[i]}/${models[j]}.ranklist"  --metrics+recip_rank --metrics+ndcg20 | awk 'NR==2{print $2}' | tr -d '\n' >> "${embedding_size}.csv"
     echo "," >> "${embedding_size}.csv"
-    ./galago-3.16-bin/bin/galago eval --judgments= "/home/share/yinxiangkun/transformed/${dataset[i]}/seq_min_count5/seq_query_split/test.qrels" --runs+ "/home/share/yinxiangkun/saved/${embedding_size}/${dataset[i]}/${models[j]}.ranklist"  --metrics+recip_rank --metrics+ndcg20 | awk 'NR==2{print $3}' | tr '\n' '' >> "${embedding_size}.csv"
+    ./galago-3.16-bin/bin/galago eval --judgments= "/home/share/yinxiangkun/transformed/${dataset[i]}/seq_min_count5/seq_query_split/test.qrels" --runs+ "/home/share/yinxiangkun/saved/${embedding_size}/${dataset[i]}/${models[j]}.ranklist"  --metrics+recip_rank --metrics+ndcg20 | awk 'NR==2{print $3}' | tr -d '\n' >> "${embedding_size}.csv"
   done
 done
