be2e3444b215d7c40379c2a36d8b1671d33a635a
ptyin <peteryin1604@gmail.com>
2020-09-18 09:41:45 +0800

    remove clipped_norm

--- esrt/models/aem.py
+++ esrt/models/aem.py
@@ -133,9 +133,7 @@ class AEM(BaseModel):
         opt = tf.train.AdamOptimizer(self.learning_rate)
         self.gradients = tf.gradients(self.loss, params)
 
-        self.clipped_gradients, self.norm = tf.clip_by_global_norm(self.gradients,
-                                                                 self.max_gradient_norm)
-        return opt.apply_gradients(zip(self.clipped_gradients, params),
+        return opt.apply_gradients(zip(self.gradients, params),
                                          global_step=self.global_step)
 
     def step(self, session, input_feed, forward_only, file_writer=None, test_mode='product_scores'):
